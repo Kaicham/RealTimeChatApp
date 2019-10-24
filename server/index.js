7ec1d88be1809d460fa33a8c9763ba7e2d1f781a
Kaique Santana <kaikham@hotmail.com>
2019-10-23 23:43:41 -0300

    base configuration of socket.io + Join component

--- server/index.js
+++ server/index.js
@@ -10,6 +10,14 @@ const io = socketio(server);
 const router = require('./router')
 const PORT = 8000
 
+io.on('connection', (socket) => {
+    console.log('We have a new connection!');
+
+    socket.on('disconnect', () => {
+        console.log('User has left!')
+    })
+})
+
 app.use(router)
 
 server.listen(PORT, () => console.log(`Server started at port ${PORT}`))
